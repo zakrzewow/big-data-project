b19e18e70b568ab0c3832ed70d82d714248e5b18
Kacper Skonieczka <kacper.skonieczka@respect.energy>
2024-01-09 23:24:55 +0100

    add new important files

--- hadoop/gefs_hadoop_init.sh
+++ hadoop/gefs_hadoop_init.sh
@@ -1,3 +1,6 @@
 hadoop fs -mkdir /user/bigdataproject
 hadoop fs -mkdir /user/bigdataproject/gefs
 hadoop fs -mkdir /user/bigdataproject/gefs/raw
+
+hdfs dfs -mkdir /user/project/germany_prices
+hdfs dfs -mkdir /user/project/windOffshore
